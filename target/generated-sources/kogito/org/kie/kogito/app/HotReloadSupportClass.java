3a37e604ebf699f23c46946dcd3930ec99e2d216
NikitaPorwal21 <porwal.nikita@gmail.com>
2023-10-16 14:53:42 +0530

    adding yaml file

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "5d111b6c-06cb-4778-bdb3-ab12b3cf8869";
+private static final String ID = "8999eff9-662e-48cb-8631-00eb2e24b0ba";
 }
